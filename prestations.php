ae4a87ff875d8bb460c0f95fbaa2da3900e9ab1a
Tania <tania.juspin@gmail.com>
2020-05-05 18:13:39 +0200

    mise en place fichiers, début du front end

---
+++ prestations.php
@@ -0,0 +1,6 @@
+<?php
+    $page_title = "Vos prestations";
+    require "php/view/header.php";
+    require "php/view/section_prestations.php";
+    require "php/view/footer.php";
+?>
---
+++ visites.php
@@ -0,0 +1,6 @@
+<?php
+    $page_title = "Vos visites";
+    require "php/view/header.php";
+    require "php/view/section_visites.php";
+    require "php/view/footer.php";
+?>
